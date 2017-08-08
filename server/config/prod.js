890fb4d62c1d2ee5e679b1560f773d65e5d4a196
Dan Youberg <dyouberg@invaluable.com>
2017-08-08 16:24:51 -0400

    Finished auth flow

---
+++ server/config/prod.js
@@ -0,0 +1,7 @@
+// prod.js - do not commit
+module.exports = {
+  cookieKey: process.env.COOKIE_KEY,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI
+};
