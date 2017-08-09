48a0e62d2b67155c2c4e2b21095e21f96b54244b
Dan Youberg <dyouberg@invaluable.com>
2017-08-09 17:07:25 -0400

    Restructured

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+// dev.js - Do not commit this!
+module.exports = {
+  cookieKey: 'hgkebtwdv8sak2143i1g4kjbfsihajb123k4',
+  googleClientID: '377836340716-tshvdupjfoucae3d5ha9icfakaotq82g.apps.googleusercontent.com',
+  googleClientSecret: 'ZmutYIUy0YCYyVZ16wKlRxPZ',
+  mongoURI: 'mongodb://dyouberg:password@ds015713.mlab.com:15713/emaily-dev'
+};
